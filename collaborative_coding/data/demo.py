746c1845af7f8833db7b6c1eb647f086847b8abb
Gina M. Notaro <gmnotaro@gmail.com>
2018-03-07 13:15:38 -0500

    new demo.py file

--- collaborative_coding/data/demo.py
+++ collaborative_coding/data/demo.py
@@ -19,14 +19,92 @@ ax.add_patch(
 
 # Emily's code
 
+ax.add_patch(
+    patches.Rectangle(
+        (0.15, 0.15),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#328D62",
+    )
+)
+
 # Kirsten's code
+ax.add_patch(
+    patches.Rectangle(
+        (0.15, 0.4),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#7FB79A",
+    )
+)
+
+ax.add_patch(
+    patches.Rectangle(
+        (0.15, 0.65),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#CCE1D8",
+    )
+)
 
 # Lucy's code
+ax.add_patch(
+    patches.Rectangle(
+        (0.4, 0.15),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#197F50",
+    )
+)
+
+ax.add_patch(
+    patches.Rectangle(
+        (0.4, 0.4),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#66A98A",
+    )
+)
+
 
 # Andy's code
+ax.add_patch(
+    patches.Rectangle(
+        (0.4, 0.65),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#B2D4C4",
+    )
+)
 
+ax.add_patch(
+    patches.Rectangle(
+        (0.65, 0.15),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#00713D",
+    )
+)
 # Jeremy's code
+ax.add_patch(
+    patches.Rectangle(
+        (0.65, 0.4),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#4E9B79",
+    )
+)
+
+ax.add_patch(
+    patches.Rectangle(
+        (0.65, 0.65),   # (x,y)
+        0.2,          # width
+        0.2,          # height
+        facecolor="#99C6B1",
+    )
+)
 
 # Gina's code
+
 
 plt.show()
